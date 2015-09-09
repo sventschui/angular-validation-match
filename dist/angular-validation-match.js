249737fb2faf5d5f97821e4b44d4c55401283ec0
Sven Tschui <sven.tschui@axa-winterthur.ch>
2015-09-09 10:34:55 +0200

    build & copyBuild

--- dist/angular-validation-match.js
+++ dist/angular-validation-match.js
@@ -1,7 +1,7 @@
 /*!
  * angular-validation-match
  * Checks if one input matches another
- * @version v1.5.1
+ * @version v1.6.0
  * @link https://github.com/TheSharpieOne/angular-validation-match
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
@@ -10,6 +10,7 @@
 angular.module('validation.match', []);
 
 angular.module('validation.match').directive('match', match);
+angular.module('validation.match').directive('mismatch', mismatch);
 
 function match ($parse) {
     return {
@@ -49,4 +50,43 @@ function match ($parse) {
     };
 }
 match.$inject = ["$parse"];
+
+function mismatch ($parse) {
+    return {
+        require: '?ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attrs, ctrl) {
+            if(!ctrl) {
+                if(console && console.warn){
+                    console.warn('Mismatch validation requires ngModel to be on the element');
+                }
+                return;
+            }
+
+            var mismatchGetter = $parse(attrs.mismatch);
+            var caselessGetter = $parse(attrs.mismatchCaseless);
+
+            scope.$watch(getMismatchValue, function(){
+                ctrl.$$parseAndValidate();
+            });
+
+            ctrl.$validators.mismatch = function(){
+                var mismatch = getMismatchValue();
+                if(caselessGetter(scope) && angular.isString(mismatch) && angular.isString(ctrl.$viewValue)){
+                    return ctrl.$viewValue.toLowerCase() === mismatch.toLowerCase();
+                }
+                return ctrl.$viewValue === mismatch;
+            };
+
+            function getMismatchValue(){
+                var mismatch = mismatchGetter(scope);
+                if(angular.isObject(mismatch) && mismatch.hasOwnProperty('$viewValue')){
+                    mismatch = mismatch.$viewValue;
+                }
+                return match;
+            }
+        }
+    };
+}
+mismatch.$inject = ["$parse"];
 })(window, window.angular);
